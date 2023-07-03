4f5706f58acfb9ff6308ed471bbd7238d9291d65
Igor Sosnowicz <igor.sosnowicz@gmail.com>
2023-07-03 08:18:24 +0000

    Minor fixes

--- backend/src/DatabaseConfiguration.php
+++ backend/src/DatabaseConfiguration.php
@@ -4,6 +4,6 @@ class DatabaseConfiguration
 {
     public const SERVER = 'localhost';
     public const USERNAME = 'root';
-    public const PASSWORD = 'Fss2PS1E';
+    public const PASSWORD = '';
     public const DATABASE_NAME = 'in_lenses';
 }
